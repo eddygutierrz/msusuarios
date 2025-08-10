a2334ba5daaedaa9c634f7ad31f4070746de3519
eddy.gutierrez <eddy.gutierrez@remtools.mx>
2025-08-09 19:46:49 -0600

    Fix: Ajuste para forzar el escaneo de entidades

--- src/main/java/com/impulsofirme/msusuarios/app/MsusuariosApplication.java
+++ src/main/java/com/impulsofirme/msusuarios/app/MsusuariosApplication.java
@@ -2,9 +2,13 @@ package com.impulsofirme.msusuarios.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.impulsofirme.msusuarios.app.entity")
+@EnableJpaRepositories(basePackages = "com.impulsofirme.msusuarios.app.repository")
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class MsusuariosApplication {
 
