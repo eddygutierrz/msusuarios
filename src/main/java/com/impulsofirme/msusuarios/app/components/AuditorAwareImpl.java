b0b3b774f3c4c279edc4cc107bffd92c6470c8d6
eddy.gutierrez <eddy.gutierrez@remtools.mx>
2025-08-16 23:19:37 -0600

    Feature: Ajustes al auditor

--- src/main/java/com/impulsofirme/msusuarios/app/components/AuditorAwareImpl.java
+++ src/main/java/com/impulsofirme/msusuarios/app/components/AuditorAwareImpl.java
@@ -4,13 +4,15 @@ import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component("auditorAware")
 public class AuditorAwareImpl implements AuditorAware<String>{
     @Override
     public @NonNull Optional<String> getCurrentAuditor() {
-        // Aquí puedes conectar con Spring Security si quieres
-        return Optional.of("system"); // Puedes poner "admin", "backend", o luego integrar con JWT
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+            .map(authentication -> authentication.getName()) // Obtén el nombre del usuario
+            .or(() -> Optional.of("system")); // Valor predeterminado si no hay usuario autenticado
     }
 }
